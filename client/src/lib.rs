8fc71cd2248a526d7cce0b232d027507b2469f68
Adam Zegelin <adam@zegelin.com>
2024-01-09 01:34:59 +1100

    Disable WIP client code for now.

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -127,72 +127,72 @@ impl Client {
 
         
 
-        mqtt.lock().unwrap().subscribe_json(format!("{}zones", topic_base), QoS::AtLeastOnce, {
-            let mqtt = mqtt.clone();
+        // mqtt.lock().unwrap().subscribe_json(format!("{}zones", topic_base), QoS::AtLeastOnce, {
+        //     let mqtt = mqtt.clone();
 
-            move |publish: &Publish, zones: Vec<String>| {
-                let zones = zones.into_iter()
-                    .map(|zone| ZoneId::from_str(&zone))
-                    .collect::<Result<Vec<ZoneId>, ZoneIdError>>();
+        //     move |publish: &Publish, zones: Vec<String>| {
+        //         let zones = zones.into_iter()
+        //             .map(|zone| ZoneId::from_str(&zone))
+        //             .collect::<Result<Vec<ZoneId>, ZoneIdError>>();
 
-                let zones = match zones {
-                    Ok(zones) => zones,
-                    Err(e) => {
-                        log::error!("{}: {}", publish.topic, e);
-                        updates_send.send(StatusUpdate::Error()).expect("send on updates_send");
-                        return;
-                    }
-                };
+        //         let zones = match zones {
+        //             Ok(zones) => zones,
+        //             Err(e) => {
+        //                 log::error!("{}: {}", publish.topic, e);
+        //                 updates_send.send(StatusUpdate::Error()).expect("send on updates_send");
+        //                 return;
+        //             }
+        //         };
 
-                updates_send.send(StatusUpdate::AvailableZones(zones.clone())).expect("send on updates_send");
+        //         updates_send.send(StatusUpdate::AvailableZones(zones.clone())).expect("send on updates_send");
 
-                // TODO: implement unsubscribe for zones that are no longer in the available zones list
+        //         // TODO: implement unsubscribe for zones that are no longer in the available zones list
                 
 
-                let mut mqtt = mqtt.lock().unwrap();
+        //         let mut mqtt = mqtt.lock().unwrap();
 
-                for zone in zones {
-                    dbg!(zone);
-                    let topic_base = format!("{}zone/{}/", topic_base, zone);
+        //         for zone in zones {
+        //             dbg!(zone);
+        //             let topic_base = format!("{}zone/{}/", topic_base, zone);
 
-                    mqtt.subscribe_json(format!("{}name", topic_base), QoS::AtLeastOnce, {
-                        let updates_send = updates_send.clone();
+        //             mqtt.subscribe_json(format!("{}name", topic_base), QoS::AtLeastOnce, {
+        //                 let updates_send = updates_send.clone();
 
-                        move |_publish: &Publish, name: String| {
-                            updates_send.send(StatusUpdate::ZoneMeta(zone, ZoneMeta::Name(name)))
-                                .expect("send on updates_send");
-                        }
-                    }).unwrap();
+        //                 move |_publish: &Publish, name: String| {
+        //                     updates_send.send(StatusUpdate::ZoneMeta(zone, ZoneMeta::Name(name)))
+        //                         .expect("send on updates_send");
+        //                 }
+        //             }).unwrap();
 
-                    // System and Amp zones don't receive attribute status updates
-                    // is there a way to do if-let-or? or something better
-                    if let ZoneId::Zone { amp: _, zone: _ } = zone {
-                    } else {
-                        continue;
-                    }
+        //             // System and Amp zones don't receive attribute status updates
+        //             // is there a way to do if-let-or? or something better
+        //             if let ZoneId::Zone { amp: _, zone: _ } = zone {
+        //             } else {
+        //                 continue;
+        //             }
 
-                    mqtt.subscribe_json(format!("{}public-announcement", topic_base), QoS::AtLeastOnce, {
-                        let updates_send = updates_send.clone();
+        //             mqtt.subscribe_json(format!("{}public-announcement", topic_base), QoS::AtLeastOnce, {
+        //                 let updates_send = updates_send.clone();
 
-                        move |_publish: &Publish, pa: bool| {
-                            updates_send.send(StatusUpdate::ZoneAttribute(zone, ZoneAttribute::PublicAnnouncement(pa)))
-                                .expect("send on updates_send");
-                        }
-                    }).unwrap();
+        //                 move |_publish: &Publish, pa: bool| {
+        //                     updates_send.send(StatusUpdate::ZoneAttribute(zone, ZoneAttribute::PublicAnnouncement(pa)))
+        //                         .expect("send on updates_send");
+        //                 }
+        //             }).unwrap();
 
-                    mqtt.subscribe_json(format!("{}volume", topic_base), QoS::AtLeastOnce, {
-                        let updates_send = updates_send.clone();
+        //             mqtt.subscribe_json(format!("{}volume", topic_base), QoS::AtLeastOnce, {
+        //                 let updates_send = updates_send.clone();
 
-                        move |_publish: &Publish, volume: u8| {
-                            updates_send.send(StatusUpdate::ZoneAttribute(zone, ZoneAttribute::Volume(volume)))
-                                .expect("send on updates_send");
-                        }
-                    }).unwrap();
-                }
+        //                 move |_publish: &Publish, volume: u8| {
+        //                     updates_send.send(StatusUpdate::ZoneAttribute(zone, ZoneAttribute::Volume(volume)))
+        //                         .expect("send on updates_send");
+        //                 }
+        //             }).unwrap();
+        //         }
 
                 
-            }
-        }).unwrap();
+        //     }
+        // }).unwrap();
 
         // handle out-of-order zones:  status/zones contains list of active zones, however we may get messages
         // about zones we dont care about. how to handle?
